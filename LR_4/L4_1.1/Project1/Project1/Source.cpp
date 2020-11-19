9c04ad08a07efa71738df4d9171959b718d8e3c1
AlSergachev <sanyasergachev@yandex.by>
2020-11-19 17:07:10 +0300

    3 tests

--- LR_4/L4_1.1/Project1/Project1/Source.cpp
+++ LR_4/L4_1.1/Project1/Project1/Source.cpp
@@ -8,15 +8,17 @@ using namespace std;
 
 int main(int count, char* array[])
 {
-    ifstream inf(array[1]);
-    ofstream outf(array[2]);
-
-
     if (count != 3)
     {
         cout << "Count of arguments must be equal 2\n" << endl;
         return 1;
     }
+
+    ifstream inf(array[1]);
+    ofstream outf(array[2]);
+
+
+    
 
     if (!inf)
     {
@@ -52,6 +54,30 @@ int main(int count, char* array[])
 
     inf.close();
     outf.close();
+
+    /*
+
+        
+
+        ::Test 3
+% MyProgram % first.txt && goto err
+echo "Test 3 Passed"
+
+        ::Test 4
+        % MyProgram % &&goto err
+        echo "Test 4 Passed"
+
+        ::Test 5
+        % MyProgram % error.txt && goto err
+        echo "Test 5 Passed"
+
+        ::Test 6
+        % MyProgram % error.txt Copy.txt && goto err
+        echo "Test 6 Passed"
+
+        ::Test 7
+        % MyProgram % first.txt error.txt && goto err
+        echo "Test 7 Passed"*/
 
     return 0;
 }
